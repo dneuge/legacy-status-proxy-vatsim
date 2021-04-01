a13d6756eafde51f94716282a3547fbce6afb029
Daniel Neugebauer <dneuge@energiequant.de>
2021-04-01 19:03:22 +0200

    use application version for user-agent

--- src/main/java/de/energiequant/vatsim/compatibility/legacyproxy/AppConstants.java
+++ src/main/java/de/energiequant/vatsim/compatibility/legacyproxy/AppConstants.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import org.vatplanner.dataformats.vatsimpublic.parser.DataFileFormat;
 
 public class AppConstants {
-    public static final String USER_AGENT = "LegacyStatusProxy/0.1";
+    public static final String USER_AGENT = "LegacyStatusProxy/" + Main.getApplicationVersion();
     public static final Duration EXTERNAL_REQUEST_TIMEOUT = Duration.ofSeconds(30);
     public static final DataFileFormat UPSTREAM_DATA_FILE_FORMAT = DataFileFormat.JSON3;
 }
