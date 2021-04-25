096bd904f5e97659e159f1224b67d5a904c96b98
Daniel Neugebauer <dneuge@energiequant.de>
2021-04-25 19:59:39 +0200

    increased warn duration for integrated VAT-Spy data to 270 days

--- src/main/java/de/energiequant/vatsim/compatibility/legacyproxy/AppConstants.java
+++ src/main/java/de/energiequant/vatsim/compatibility/legacyproxy/AppConstants.java
@@ -8,7 +8,7 @@ public class AppConstants {
     public static final String USER_AGENT = "LegacyStatusProxy/" + Main.getApplicationVersion();
     public static final Duration EXTERNAL_REQUEST_TIMEOUT = Duration.ofSeconds(30);
     public static final DataFileFormat UPSTREAM_DATA_FILE_FORMAT = DataFileFormat.JSON3;
-    public static final Duration VATSPY_AGE_WARNING_THRESHOLD = Duration.ofDays(180);
+    public static final Duration VATSPY_AGE_WARNING_THRESHOLD = Duration.ofDays(270);
 
     public static final String DEPENDENCIES_CAPTION = "This software includes the following runtime dependencies.\nFor full author information please refer to their individual websites.\nIn alphabetical order:";
     public static final String PROJECT_DEPENDENCY_LICENSE_INTRO = "made available under ";
